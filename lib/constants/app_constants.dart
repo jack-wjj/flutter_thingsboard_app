4e643033c6054f3e6779276eb3428abec5bbf3bf
jack-wjj <61659064+jack-wjj@users.noreply.github.com>
2025-06-19 23:41:08 +0800

    Update API入口

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,5 @@
 abstract class ThingsboardAppConstants {
-  static const thingsBoardApiEndpoint = 'http://localhost:8080';
+  static const thingsBoardApiEndpoint = 'http://124.220.198.92:8080';
   static const thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
 
   static const thingsboardIOSAppSecret = 'Your app secret here';
